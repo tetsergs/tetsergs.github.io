4ad2229ba982bb68a8aa51c4fb747a0f29437439
tetsergs <tetsergs123@gmail.com>
2022-08-30 00:19:11 +0600

    First try desk GutHub

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let typed = new Typed('#typed', { // Тут id того блока, в которм будет анимация
+    stringsElement: '#typed-strings', // Тут id блока из которого берем строки для анимации
+    typeSpeed: 100, // Скорость печати
+    startDelay: 500, // Задержка перед стартом анимации
+    backSpeed: 50, // Скорость удаления
+    loop: true // Указываем, повторять ли анимацию
+});
+
+//simple parallax for items section
+let image = document.getElementsByClassName('parallax');
+new simpleParallax(image, {
+    orientation: 'down',
+	delay: .6,
+	transition: 'cubic-bezier(0,0,0,1)'
+});
